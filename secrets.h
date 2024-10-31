824936dfa18e00835731f4c46947ed7f28d7f01e
jddb2208 <55630889+jddb2208@users.noreply.github.com>
2024-10-30 19:10:47 -0600

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,10 @@
+// Adapted from thingspeak-arduino github
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "TP-Link_2A61"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "56494535"  // LIMA TP Link password 
+
+#define SECRET_CH_ID 2522041			// replace with your channel number
+#define SECRET_WRITE_APIKEY "JY2YI0RF1F1O1EEY"   // replace with your channel write API Key
+#define SECRET_READ_APIKEY "4TDJ9OX0O5WILXA2" // replace with your channel read API Key
